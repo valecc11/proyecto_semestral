a49f7311eda728fbb4932cab0b4bdae1b5528674
valecc11 <vale.castroc@duocuc.cl>
2023-05-01 02:23:13 -0400

    e

--- js/jquery.js
+++ js/jquery.js
@@ -3,76 +3,99 @@ $(document).ready(function () {
     $("#form1").submit(function (e) {
         e.preventDefault();
         var nombre = $("#name").val();
-        var correo = $("#email").val();
+        var correoc = $("#email").val();
         var clave = $("#password").val();
         var fecha = $("#fecha").val();
 
         let msjMostrar = "";
         let enviar = false;
 
-        if (nombre.trim().length < 4 || nombre.trim().length > 10) {                        //validar nombre
-            msjMostrar = msjMostrar + "El nombre debe tener entre 4 y 10 caracteres";
-            enviar = true;
-        }
-        else if (/\d/.test(nombre)) {
-            msjMostrar += "<br> El nombre no puede contener números"
-            enviar = true;
-        }
-
-        var letra = nombre.trim().charAt(0);
-        if (!esMayuscula(letra)) {
-            msjMostrar += "<br>El nombre debe comenzar con mayúscula";
-            enviar = true;
-        }
-        var correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;                                     //validar correo, aun falta que funcione
-        if (!correoRegex.test(correo)) {
-            msjMostrar += "<br> Ingrese una dirección de correo válida"
-            enviar = true;
-        }
+        if (nombre === '') {
+            msjMostrar += "El campo Nombre debe ser rellenado";
+            
 
 
-        if (clave.trim().length < 8) {                                                      //validar contraseña, tiene un minimo de 8 caracteres, un numero, caracter especial, minus, mayus 
-            msjMostrar += "<br>La contraseña debe tener al menos 8 carácteres";
-            enviar = true;
-        }
-        else if (!/\d/.test(clave)) {
-            msjMostrar += "<br>La contraseña debe contener al menos un número";
-            enviar = true;
-        }
-        else if (!/[a-z]/.test(clave)) {
-            msjMostrar += "<br>La contraseña debe contener al menos una letra minúscula";
-            enviar = true;
-
-        }
-        else if (!/[A-Z]/.test(clave)) {
-            msjMostrar += "<br>La contraseña debe contener al menos una letra mayúscula";
-            enviar = true;
-
-        }
-        else if (!/[!@#$&*]/.test(clave)) {
-            msjMostrar += "<br>La contraseña debe contener al menos un carácter especial";
-            enviar = true;
-
-        }
-
-        if (Date.parse(fecha)) {                                                             //Validar fecha de nacimiento, no permite menores de edad 
-            var edadMilisegundos = Date.now() - Date.parse(fecha);
-            var edad = new Date(edadMilisegundos).getUTCFullYear() - 1970;
-            if (edad >= 18) {
-                // El usuario tiene al menos 18 años
-            } else {
-                msjMostrar += "<br>Debes tener al menos 18 años para registrarte"
+        } else {
+            if (nombre.trim().length < 4 || nombre.trim().length > 10) {                        //validar nombre
+                msjMostrar = msjMostrar + "El nombre debe tener entre 4 y 10 caracteres";
                 enviar = true;
             }
-        } else {
+            else if (/\d/.test(nombre)) {
+                msjMostrar += "<br> El nombre no puede contener números";
+                enviar = true;
+            }
 
+            var letra = nombre.trim().charAt(0);
+            if (!esMayuscula(letra)) {
+                msjMostrar += "<br>El nombre debe comenzar con mayúscula";
+                enviar = true;
+            }
         }
+        if (correoc === '') {
+            msjMostrar += "<br>El campo Correo debe ser rellenado";
+
+        } else {
+            if (isValidEmail(correoc)) {
+                enviar = true;
+
+            } else {
+                // El correo no es válido, mostrar un mensaje de error
+                msjMostrar += "<br> El correo ingresado no es válido";
+                enviar = true;
+
+            }
+        }
+        if (clave === '') {
+            msjMostrar += "<br>El campo Contraseña debe ser rellenado";
+
+        } else {
+            if (clave.trim().length < 8) {
+                msjMostrar += "<br>La contraseña debe tener al menos 8 carácteres";
+                enviar = true;
+            }
+            else if (!/\d/.test(clave)) {
+                msjMostrar += "<br>La contraseña debe contener al menos un número";
+                enviar = true;
+            }
+            else if (!/[a-z]/.test(clave)) {
+                msjMostrar += "<br>La contraseña debe contener al menos una letra minúscula";
+                enviar = true;
+
+            }
+            else if (!/[A-Z]/.test(clave)) {
+                msjMostrar += "<br>La contraseña debe contener al menos una letra mayúscula";
+                enviar = true;
+
+            }
+            else if (!/[!@#$&*]/.test(clave)) {
+                msjMostrar += "<br>La contraseña debe contener al menos un carácter especial";
+                enviar = true;
+
+            }
+        }
+        if (fecha === '') {
+            msjMostrar += "<br>El campo Fecha debe ser rellenado";
+
+        } else {
+            if (Date.parse(fecha)) {
+                var edadMilisegundos = Date.now() - Date.parse(fecha);
+                var edad = new Date(edadMilisegundos).getUTCFullYear() - 1970;
+                if (edad >= 18) {
+                    // El usuario tiene al menos 18 años
+                    enviar = true;
+                } else {
+                    msjMostrar += "<br>Debes tener al menos 18 años para registrarte";
+                    enviar = true;
+                }
+            }
+        }
+
 
         if (enviar) {
             $("#warnings").html(msjMostrar);
         }
         else {
-            $("#warnings").html("Enviado");
+            $("#warnings").html("Se ha registrado correctamente");
         }
 
 
@@ -92,6 +115,13 @@ $(document).ready(function () {
             return false;
         }
     }
+    //Función que permite validar si el correo lleva @ y .
+    function isValidEmail(correoc) {
+        var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return pattern.test(correoc);
+
+    }
+
 
 
 
@@ -103,7 +133,7 @@ $(document).ready(function () {
 $(document).ready(function () {
     $("#form2").submit(function (e) {
         e.preventDefault();
-        
+
         var correo = $("#correo").val();
         var clave = $("#password").val();
 
@@ -118,24 +148,24 @@ $(document).ready(function () {
             });
             msjMostrar += "Bienvenido, haz click nuevamente!";
             enviar = true;
-        
-        } else{
+
+        } else {
             // La palabra no es correcta, mostrar un mensaje de error
             msjMostrar += "<br> El correo ingresado o contraseña son incorrectos";
-            
+
 
         }
-      
-            
 
-        
+
+
+
         if (isValidEmail(correo)) {
             enviar = true;
 
         } else {
             // El correo no es válido, mostrar un mensaje de error
             msjMostrar += "<br> Incluye un @ para poder continuar"
-            
+
 
         }
 
@@ -173,7 +203,7 @@ $(document).ready(function () {
             enviar = true;
         }
         else if (/\d/.test(nombre)) {
-            msjMostrar += "<br> El nombre no puede contener números"
+            msjMostrar += "<br> El nombre no puede contener números";
             enviar = true;
         }
 
@@ -187,7 +217,7 @@ $(document).ready(function () {
 
         } else {
             // El correo no es válido, mostrar un mensaje de error
-            msjMostrar += "<br> El correo ingresado no es válido"
+            msjMostrar += "<br> El correo ingresado no es válido";
 
         }
 
@@ -200,7 +230,7 @@ $(document).ready(function () {
             if (edad >= 18) {
                 // El usuario tiene al menos 18 años
             } else {
-                msjMostrar += "<br>Debes tener al menos 18 años para registrarte"
+                msjMostrar += "<br>Debes tener al menos 18 años para registrarte";
                 enviar = true;
             }
         } else {
@@ -251,7 +281,7 @@ $(document).ready(function () {
 $(document).ready(function () {
     $("#form4").submit(function (e) {
         e.preventDefault();
-        
+
         var antigua = $("#current-password").val();
         var newPassword = $('#new-password').val();
         var confirmPassword = $('#confirm-password').val();
@@ -259,31 +289,31 @@ $(document).ready(function () {
         let msjMostrar = "";
         let enviar = false;
 
-    
-        // Verificar longitud mínima
-        
 
-        if( antigua === 'Taylorswift#13'){
-            
+        // Verificar longitud mínima
+
+
+        if (antigua === 'Taylorswift#13') {
+
             enviar = true;
 
             if (newPassword == confirmPassword) {
                 msjMostrar += "La contraseña se ha cambiado exitosamente";
                 enviar = true;
-                
-            }else{
+
+            } else {
                 msjMostrar += "Las contraseñas no coinciden, intente nuevamente ";
                 enviar = true;
             }
-                
 
-        }else{
-            msjMostrar +="La contraseña actual no es correcta";
+
+        } else {
+            msjMostrar += "La contraseña actual no es correcta";
             enviar = true;
-            
+
         }
         //validar contraseña
-        if (newPassword.trim().length < 8) {                                                      
+        if (newPassword.trim().length < 8) {
             msjMostrar += "<br>La contraseña debe tener al menos 8 carácteres";
             enviar = true;
         }
@@ -307,8 +337,8 @@ $(document).ready(function () {
 
         }
         //Validar si son iguales
-        
-    
+
+
 
         if (enviar) {
             $("#warnings").html(msjMostrar);
